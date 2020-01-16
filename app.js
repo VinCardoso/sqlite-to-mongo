9250381ba9536fbd29a2608723a674d69b24e7e0
Vinicius Cardoso <viniciuscc10@gmail.com>
2020-01-16 08:55:17 -0300

    v0.0.5 - Send to MongoDB every 50

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-const db = require('better-sqlite3')('../can-bus-tcc/database.db', { verbose: console.log });
-const request = require('request');
 const dotenv = require('dotenv');
-
-// Setup DotEnv
 dotenv.config();
+
+// const db = require('better-sqlite3')(process.env.SQLITE_FILE, { verbose: console.log });
+const db = require('better-sqlite3')(process.env.SQLITE_FILE)
+const request = require('request');
+
+const options = {
+    url: process.env.API_URL,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    json: true
+};
+
+const query_update_id = db.prepare('UPDATE data SET upload=1 WHERE id = ?');
 
 const update = () => {
 
-    const row = db.prepare('SELECT * FROM data WHERE upload = ?').get(0);
+    const list = db.prepare('SELECT * FROM data WHERE upload = ? LIMIT 50').all(0);
 
-    var json = {
-        "ts": row.ts,
-        "ts_u": row.ts_u,
-        "ts_complete": row.ts_complete,
-        "data_time": row.data_time,
-        "mod": row.module,
-        "info": row.info
-    };
+    var json = list.map(u => ({ 
+        ts: u.ts, 
+        ts_u: u.ts_u,
+        ts_complete: u.ts_complete,
+        data_time: u.data_time,
+        mod: u.module,
+        info: u.info
+    }));
 
-    var options = {
-        url: process.env.API_URL,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: true,
-        body: json
-    };
+    options.body = json;
 
     request.post(options, (err, res, body) => {
         if (err) {
             return console.log(err);
         }
-        const query = db.prepare('UPDATE data SET upload=1 WHERE id = ?');
-        query.run(row.id);
-        console.log("send: " + row.id);
+        list.forEach(i => {
+            query_update_id.run(i.id)
+            console.log({ id_change: i.id});
+        });
+        
+        setTimeout(update, 100);
+    
     });
 }
 
-
 update();
-
-setInterval(update, 100);
