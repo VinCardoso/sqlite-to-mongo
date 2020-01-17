e9938cdd0edf1487333d543ac79f2ff7a1ed3f2b
Vinicius Cardoso <viniciuscc10@gmail.com>
2020-01-16 23:18:21 -0300

    v0.0.6 - Update to Create a new DataBase in SQLite to store the log lines that already are synced.

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+// Import Librarys
+const Database = require('better-sqlite3');
+const request = require('request');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
-// const db = require('better-sqlite3')(process.env.SQLITE_FILE, { verbose: console.log });
-const db = require('better-sqlite3')(process.env.SQLITE_FILE)
-const request = require('request');
+// Select the Log Database File
+const db = Database(process.env.SQLITE_LOG_DB);
+const db_file_name = process.env.SQLITE_LOG_DB;
 
+const sendPerRequest = 1000; // How many log lines send each request?
+
+// Create SYNC Database and Search Last ID Updated
+const sync_db = new Database(process.env.SQLITE_SYNC_DB);
+sync_db.exec("CREATE TABLE if not exists sync (id INTEGER PRIMARY KEY AUTOINCREMENT, db_file TEXT, id_on_file INTEGER, upload INTEGER )");
+sync_db.exec("CREATE TABLE if not exists last_sync (id INTEGER PRIMARY KEY AUTOINCREMENT, db_file TEXT UNIQUE, last_id_sync INTEGER )");
+sync_db.prepare('INSERT OR IGNORE INTO last_sync (db_file, last_id_sync) VALUES (?, ?)').run(db_file_name, 0);
+
+var l = sync_db.prepare('SELECT last_id_sync FROM last_sync WHERE db_file = ?').get(db_file_name);
+
+// Prepare Querys
+const query_add_to_list = sync_db.prepare('INSERT INTO sync(db_file, id_on_file, upload) VALUES(?, ?, ?)');
+const query_update_last_id_sync = sync_db.prepare('UPDATE last_sync SET last_id_sync = ? WHERE db_file = ?');
+
+// Define Request Default
 const options = {
     url: process.env.API_URL,
     method: 'POST',
@@ -14,11 +33,9 @@ const options = {
     json: true
 };
 
-const query_update_id = db.prepare('UPDATE data SET upload=1 WHERE id = ?');
-
 const update = () => {
 
-    const list = db.prepare('SELECT * FROM data WHERE upload = ? LIMIT 50').all(0);
+    const list = db.prepare('SELECT * FROM data WHERE id > ? LIMIT ?').all(l.last_id_sync, sendPerRequest);
 
     var json = list.map(u => ({ 
         ts: u.ts, 
@@ -36,7 +53,9 @@ const update = () => {
             return console.log(err);
         }
         list.forEach(i => {
-            query_update_id.run(i.id)
+            l.last_id_sync = i.id;
+            query_add_to_list.run(db_file_name, i.id, 1)
+            query_update_last_id_sync.run(l.last_id_sync, db_file_name)
             console.log({ id_change: i.id});
         });
         
