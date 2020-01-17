d3df1c8516cc5fd59d01e4fce079c87a1ac86b64
Vinicius Cardoso <viniciuscc10@gmail.com>
2020-01-17 00:39:44 -0300

    v0.0.8 - Just register one time on sync database per request

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ const sendPerRequest = process.env.LOG_LINES_TO_UPDATE; // How many log lines se
 
 // Create SYNC Database and Search Last ID Updated
 const sync_db = new Database(process.env.SQLITE_SYNC_DB);
-sync_db.exec("CREATE TABLE if not exists sync (id INTEGER PRIMARY KEY AUTOINCREMENT, db_file TEXT, id_on_file INTEGER, upload INTEGER )");
+// sync_db.exec("CREATE TABLE if not exists sync (id INTEGER PRIMARY KEY AUTOINCREMENT, db_file TEXT, id_on_file INTEGER, upload INTEGER )");
 sync_db.exec("CREATE TABLE if not exists last_sync (id INTEGER PRIMARY KEY AUTOINCREMENT, db_file TEXT UNIQUE, last_id_sync INTEGER )");
 sync_db.prepare('INSERT OR IGNORE INTO last_sync (db_file, last_id_sync) VALUES (?, ?)').run(db_file_name, 0);
 
 var l = sync_db.prepare('SELECT last_id_sync FROM last_sync WHERE db_file = ?').get(db_file_name);
 
 // Prepare Querys
-const query_add_to_list = sync_db.prepare('INSERT INTO sync(db_file, id_on_file, upload) VALUES(?, ?, ?)');
+// const query_add_to_list = sync_db.prepare('INSERT INTO sync(db_file, id_on_file, upload) VALUES(?, ?, ?)');
 const query_update_last_id_sync = sync_db.prepare('UPDATE last_sync SET last_id_sync = ? WHERE db_file = ?');
 
 // Define Request Default
@@ -58,11 +58,16 @@ const update = () => {
         if (res.statusCode >= 400 ){
             return console.log('Data not saved');
         }else{
-            list.forEach(i => {
-                l.last_id_sync = i.id;
-                query_add_to_list.run(db_file_name, i.id, 1)
-                query_update_last_id_sync.run(l.last_id_sync, db_file_name)
-            });
+            // list.forEach(i => {
+            //     l.last_id_sync = i.id;
+            //     query_add_to_list.run(db_file_name, i.id, 1)
+            //     query_update_last_id_sync.run(l.last_id_sync, db_file_name)
+            // });
+
+            l.last_id_sync = list[list.length-1].id;
+            console.log({ storedNumber: list.length, lastItem: l.last_id_sync});
+            query_update_last_id_sync.run(l.last_id_sync, db_file_name)
+
         }
         
         setTimeout(update, 100);
